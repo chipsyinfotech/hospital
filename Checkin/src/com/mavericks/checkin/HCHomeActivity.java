9b805b4d6112871a729d6223d8457757675b02b7
chipsyinfotech <Chipsyinfotech@gmail.com>
2014-10-21 15:24:32 +0530

    home activity

--- Checkin/src/com/mavericks/checkin/HCHomeActivity.java
+++ Checkin/src/com/mavericks/checkin/HCHomeActivity.java
@@ -1,42 +1,66 @@
 package com.mavericks.checkin;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.apache.http.NameValuePair;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Spinner;
 import android.widget.TextView;
 
-import com.mavericks.checkin.adapters.HCHospitalAdapter;
-import com.mavericks.checkin.holders.HCHospitalHolder;
+import com.mavericks.checkin.adapters.HCSpinnerAdapter;
+import com.mavericks.checkin.client.HCClient;
+import com.mavericks.checkin.client.HCIRequestListener;
+import com.mavericks.checkin.client.HCServerUtils;
+import com.mavericks.checkin.holders.HCLocationHolder;
+import com.mavericks.checkin.parser.HCGetHospitalParser;
+import com.mavericks.checkin.utils.HCConstants;
+import com.mavericks.checkin.utils.HCRetryDialog;
+import com.mavericks.checkin.utils.HCRetryDialog.OnRetryClickListener;
+import com.mavericks.checkin.utils.HCUtils;
 
-public class HCHomeActivity extends HCBaseActivity implements OnClickListener,
-		OnItemClickListener {
+public class HCHomeActivity extends HCBaseActivity implements OnClickListener
+		 {
 	Button mBtnlocation;
 	Button mBtnhospital;
 	ImageView mImginfo;
 	TextView mTxtproceed;
-	ArrayList<HCHospitalHolder> mholder;
-	ArrayAdapter<String> mAdapter;
+	HCSpinnerAdapter mAdapter;
+	String Hospitalid;
+	Map<String, ArrayList<HCLocationHolder>> mHospList;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
-		mBtnlocation = (Button) findViewById(R.id.btn_location);
-		mBtnhospital = (Button) findViewById(R.id.btn_hospital);
+		mBtnlocation = (Button) findViewById(R.id.btn_sel_loc);
+		mBtnhospital = (Button) findViewById(R.id.btn_sel_hos);
 		mTxtproceed = (TextView) findViewById(R.id.text_proceed);
 		mImginfo = (ImageView) findViewById(R.id.img_info);
+		
+		//set listenr
 		mImginfo.setOnClickListener(this);
 		mTxtproceed.setOnClickListener(this);
+		mBtnlocation.setOnClickListener(this);
+		mBtnhospital.setOnClickListener(this);
+		mBtnhospital.setEnabled(false);
+		
+		mAdapter = new HCSpinnerAdapter(this);
+		mHospList = new HashMap<String, ArrayList<HCLocationHolder>>();
+		
+		//fetch Location details from server
+		getLocation();
 	}
+	
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
@@ -51,19 +75,98 @@ public class HCHomeActivity extends HCBaseActivity implements OnClickListener,
 			startActivity(i);
 			finish();
 			break;
-		case R.id.btn_location:
-			location();
+		case R.id.btn_sel_loc:
+			showLocation();
+			break;
+			
+		case R.id.btn_sel_hos:
+			showHospital();
 			break;
 		default:
 			break;
 		}
 	}
 
-	private void location() {
-	}
+	/*
+	 * Fetched the Location details from the server
+	 */
+	private void getLocation() {
+		showProgressDialog("", false);
+		
+		final HCGetHospitalParser parser = new HCGetHospitalParser();
+		List<NameValuePair> formData = new ArrayList<NameValuePair>();	
+		HCClient.getInstance().request(this, HCServerUtils.REQ_GET_HOSPITAL,
+				null, null, formData, parser, new HCIRequestListener() {
 
-	@Override
-	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-
+					@Override
+					public void onComplete(int req_type, int status) {
+						hideProgressDialog();
+						if (status == HCConstants.ERROR_CODE_SUCCESS) {
+							mHospList = parser.getMapLocation();
+						} else {
+							// If we have data in database
+							HCRetryDialog dialog = new HCRetryDialog(HCHomeActivity.this,
+									HCServerUtils.REQ_GET_HOSPITAL);
+							dialog.setOnRetryClickListener(new OnRetryClickListener() {
+								
+								@Override
+								public void onRetryClick(int requestType) {
+									getLocation();
+								}
+							});
+							dialog.show(HCHomeActivity.this.getSupportFragmentManager(), 
+									HCHomeActivity.class.getSimpleName());
+						}
+					}
+				});
 	}
+	
+	/**
+	 * shows the list of Location
+	 */
+	private void showLocation() {
+		ArrayList<String> locationList = new ArrayList<String>();
+		for (String key : mHospList.keySet()) {
+		    locationList.add(key);
+		}
+		mAdapter.setData(locationList);
+		mAdapter.notifyDataSetChanged();
+		new AlertDialog.Builder(this)
+		  .setTitle("Select Location")
+		  .setAdapter(mAdapter, new DialogInterface.OnClickListener() {
+
+		    @Override
+		    public void onClick(DialogInterface dialog, int position) {	
+		    	HCUtils.Log(" On Clickk : "+mAdapter.getItem(position).toString());
+			    mBtnlocation.setText(mAdapter.getItem(position).toString());
+			    mBtnhospital.setEnabled(true);
+			   
+			    dialog.dismiss();
+		    }
+		  }).create().show();
+	}
+	
+	/**
+	 * shows the list of Hospital
+	 */
+	private void showHospital() {
+
+		mAdapter.setData(mHospList.get(mBtnlocation.getText()));
+		mAdapter.notifyDataSetChanged();
+		new AlertDialog.Builder(this)
+		  .setTitle("Select Hospital")
+		  .setAdapter(mAdapter, new DialogInterface.OnClickListener() {
+
+		   
+		    @Override
+		    public void onClick(DialogInterface dialog, int position) {
+		    	HCLocationHolder  hospital = (HCLocationHolder) mAdapter.getItem(position);
+			    mBtnhospital.setText(hospital.getmHosName());
+	 //  Bundle bundle = new Bundle();
+	//	   bundle.putParcelable(HCConstants.EXTRA_HOSPITAL_LIST,hospital);
+			    dialog.dismiss();
+		    }
+		  }).create().show();
+	}	
+	
 }
