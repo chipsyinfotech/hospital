b88212b338ab6d73858a956735621a288185529e
chipsyinfotech <Chipsyinfotech@gmail.com>
2014-10-25 16:39:37 +0530

    added visit revisit

---
+++ Checkin/src/com/mavericks/checkin/holders/HCVisitHolder.java
@@ -0,0 +1,20 @@
+package com.mavericks.checkin.holders;
+
+public class HCVisitHolder extends HCBaseHolder{
+	String user_id;
+	String transaction_id;
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getTransaction_id() {
+		return transaction_id;
+	}
+	public void setTransaction_id(String transaction_id) {
+		this.transaction_id = transaction_id;
+	}
+	
+
+}
--- Checkin/src/com/mavericks/checkin/parser/HCVisitParser.java
+++ Checkin/src/com/mavericks/checkin/parser/HCVisitParser.java
@@ -1,75 +1,84 @@
 package com.mavericks.checkin.parser;
 
+import java.io.IOException;
+import java.util.List;
 
-public abstract class HCVisitParser extends HCBaseJsonParser {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.mavericks.checkin.holders.HCBaseHolder;
+import com.mavericks.checkin.holders.HCVisitHolder;
+
+
+public  class HCVisitParser extends HCBaseJsonParser {
 
 
 	public static final String SESSION = "session";
 	public static final String USER_ID = "user_id";
 	public static final String TR_ID = "transaction_id";
-//	HCHospitalHolder visitholder;
-//	public HCVisitParser() {
-//	}
-//
-//	/*
-//	 * (non-Javadoc)
-//	 * 
-//	 * @see com.viacom18.spotlight.models.STAJsonDataModel#initialize(java.lang.
-//	 * StringBuilder)
-//	 */
-//	@Override
-//	public void initialize(StringBuilder document) throws IOException,
-//			JSONException {
-//		super.initialize(document);
-//		JSONObject obj = new JSONObject(document.toString());
-//		if (obj.has(SESSION)) {
-//			obj = obj.getJSONObject(SESSION);
-//			visitholder = new HCHospitalHolder();
-//			if (obj.has(USER_ID))
-//				visitholder.setUser_id(obj.getString(USER_ID));
-//			if (obj.has(TR_ID))
-//				visitholder.setTransaction_id(obj.getString(TR_ID));
-//		}
-//	}
-//
-//	/*
-//	 * (non-Javadoc)
-//	 * 
-//	 * @see com.viacom18.spotlight.models.STAJsonDataModel#getModelsCount()
-//	 */
-//	@Override
-//	public int getModelsCount() {
-//		return 0;
-//	}
-//
-//	/*
-//	 * (non-Javadoc)
-//	 * 
-//	 * @see com.viacom18.spotlight.models.STAJsonDataModel#getDataHolderAt(int)
-//	 */
-//	@Override
-//	public HCBaseHolder getDataHolderAt(int position) {
-//		return null;
-//	}
-//
-//	/*
-//	 * (non-Javadoc)
-//	 * 
-//	 * @see com.viacom18.spotlight.models.STAJsonDataModel#getDataHolder()
-//	 */
-//	@Override
-//	public HCBaseHolder getDataHolder() {
-//		return visitholder;
-//	}
-//
-//	/*
-//	 * (non-Javadoc)
-//	 * 
-//	 * @see com.viacom18.spotlight.models.STAJsonDataModel#getDataList()
-//	 */
-//	@Override
-//	public List<? extends HCBaseHolder> getDataList() {
-//		return null;
-//	}
+	HCVisitHolder holder;
+	public HCVisitParser() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.viacom18.spotlight.models.STAJsonDataModel#initialize(java.lang.
+	 * StringBuilder)
+	 */
+	@Override
+	public void initialize(StringBuilder document) throws IOException,
+			JSONException {
+		super.initialize(document);
+		JSONObject obj = new JSONObject(document.toString());
+		if (obj.has(SESSION)) {
+			obj = obj.getJSONObject(SESSION);
+			holder = new HCVisitHolder();
+			if (obj.has(USER_ID))
+				holder.setUser_id(obj.getString(USER_ID));
+			if (obj.has(TR_ID))
+				holder.setTransaction_id(obj.getString(TR_ID));
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.viacom18.spotlight.models.STAJsonDataModel#getModelsCount()
+	 */
+	@Override
+	public int getModelsCount() {
+		return 0;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.viacom18.spotlight.models.STAJsonDataModel#getDataHolderAt(int)
+	 */
+	@Override
+	public HCBaseHolder getDataHolderAt(int position) {
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.viacom18.spotlight.models.STAJsonDataModel#getDataHolder()
+	 */
+	@Override
+	public HCBaseHolder getDataHolder() {
+		return holder;
+		}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.viacom18.spotlight.models.STAJsonDataModel#getDataList()
+	 */
+	@Override
+	public List<? extends HCBaseHolder> getDataList() {
+		return null;
+	}
 
 }
